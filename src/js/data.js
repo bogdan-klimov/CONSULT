b75101b722531e81ac9580ef67aceea0bb511e73
bogdan-klimov <bodyaklimov333@gmail.com>
2020-11-01 21:52:48 +0200

    page_js

--- src/js/data.js
+++ src/js/data.js
@@ -78,9 +78,24 @@ const data = [
 
 const news = [
 	{
-		title: 'blog heading loksdfsdf',
-		date: '',
-		desc: 'main text',
-		img: 'src/...'
-	}
+		title: 'BLOG HEADING LOREM IPSUM IS SIMPLY DUMMY',
+		date: '11-feb-2016',
+		desc: 'Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
+		img: 'assets/img/blog-img.png',
+		btn: 'Read More'
+	},
+	{
+		title: 'BLOG HEADING LOREM IPSUM IS SIMPLY DUMMY',
+		date: '11-feb-2016',
+		desc: 'Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
+		img: 'assets/img/blog-img-2.png',
+		btn: 'Read More'
+	},
+	{
+		title: 'BLOG HEADING LOREM IPSUM IS SIMPLY DUMMY',
+		date: '11-feb-2016',
+		desc: 'Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
+		img: 'assets/img/blog-img-5.png',
+		btn: 'Read More'
+	},
 ]
--- src/js/main.js
+++ src/js/main.js
@@ -1,110 +1,198 @@
-const slider = document.getElementById("clients-list-block");
-const visible = document.getElementById("visible");
-const sliderWrapper = document.getElementById("clients-slider");
-const SLIDES_ON_PAGE = 3;
+// const slider = document.getElementById("clients-list-block");
+// const visible = document.getElementById("visible");
+// const sliderWrapper = document.getElementById("clients-slider");
+// const SLIDES_ON_PAGE = 3;
 
-const sliderBtnWrapper = document.createElement("ul");
-sliderWrapper.append(sliderBtnWrapper);
-sliderBtnWrapper.classList.add("clients-list-carousel");
+// const sliderBtnWrapper = document.createElement("ul");
+// sliderWrapper.append(sliderBtnWrapper);
+// sliderBtnWrapper.classList.add("clients-list-carousel");
 
-for (let el in data) {
-    const client = document.createElement('li');
-    client.classList.add('clients-list-block-item');
+// for (let el in data) {
+//     const client = document.createElement('li');
+//     client.classList.add('clients-list-block-item');
 
-    const img = document.createElement('img');
-    img.classList.add('client-img');
-    img.setAttribute('src', data[el].img);
-    img.setAttribute('alt', "client-img");
+//     const img = document.createElement('img');
+//     img.classList.add('client-img');
+//     img.setAttribute('src', data[el].img);
+//     img.setAttribute('alt', "client-img");
 
-    const div = document.createElement('div');
-    div.classList.add('client-description');
+//     const div = document.createElement('div');
+//     div.classList.add('client-description');
 
-    const h3 = document.createElement("h3");
-    h3.classList.add("client-description-header");
-    h3.innerText = data[el].name;
+//     const h3 = document.createElement("h3");
+//     h3.classList.add("client-description-header");
+//     h3.innerText = data[el].name;
 
-    const span = document.createElement("span");
-    span.classList.add("client-description-text");
-    span.innerText = data[el].position;
+//     const span = document.createElement("span");
+//     span.classList.add("client-description-text");
+//     span.innerText = data[el].position;
 
-    client.append(img);
-    client.append(div);
-    slider.append(client);
-    div.append(h3);
-    div.append(span);
-}
-
-
-const sliderEl = document.getElementsByClassName("clients-list-block-item");
-
-const sliderElForBtn = Math.ceil(sliderEl.length / SLIDES_ON_PAGE);
-
-for (let i = 0; i < sliderElForBtn; i++) {
-    const sliderBtn = document.createElement("li");
-    sliderBtnWrapper.append(sliderBtn);
-    sliderBtn.classList.add("clients-list-carousel-item");
-    const point = document.createElement("div");
-    sliderBtn.append(point);
-    point.classList.add("point");
-}
-
-const sliderBtns = document.getElementsByClassName("clients-list-carousel-item");
-
-const move = visible.offsetWidth;
-
-sliderBtns[0].classList.add("clients-list-carousel-item-active");
+//     client.append(img);
+//     client.append(div);
+//     slider.append(client);
+//     div.append(h3);
+//     div.append(span);
+// }
 
 
-for (let i = 0; i < sliderBtns.length; i++) {
+// const sliderEl = document.getElementsByClassName("clients-list-block-item");
 
-    sliderBtns[i].addEventListener("click", () => {
+// const sliderElForBtn = Math.ceil(sliderEl.length / SLIDES_ON_PAGE);
 
-        slider.style.left = (-move * i) + "px";
+// for (let i = 0; i < sliderElForBtn; i++) {
+//     const sliderBtn = document.createElement("li");
+//     sliderBtnWrapper.append(sliderBtn);
+//     sliderBtn.classList.add("clients-list-carousel-item");
+//     const point = document.createElement("div");
+//     sliderBtn.append(point);
+//     point.classList.add("point");
+// }
 
-        for (let j = 0; j < sliderBtns.length; j++) {
-            sliderBtns[j].className = sliderBtns[j].className.replace("clients-list-carousel-item-active", "");
-        };
+// const sliderBtns = document.getElementsByClassName("clients-list-carousel-item");
 
-        sliderBtns[i].classList.add("clients-list-carousel-item-active");
-    });
+// const move = visible.offsetWidth;
 
-};
+// sliderBtns[0].classList.add("clients-list-carousel-item-active");
 
-/////////////////////////////////////////////////////////////////////////
 
-let backgroundsBanner = document.getElementsByClassName("banner-backgrounds");
-let leftBtn = document.getElementById("left-rectangle-arrow");
-let rightBtn = document.getElementById("right-rectangle-arrow");
+// for (let i = 0; i < sliderBtns.length; i++) {
 
-const replaceClass = (item, classItem, newClassItem) => {
-    item.className = item.className.replace(classItem, newClassItem);
-}
+//     sliderBtns[i].addEventListener("click", () => {
 
-backgroundsBanner[0].classList.add("backgrounds-active");
+//         slider.style.left = (-move * i) + "px";
 
-let idx = 0;
+//         for (let j = 0; j < sliderBtns.length; j++) {
+//             sliderBtns[j].className = sliderBtns[j].className.replace("clients-list-carousel-item-active", "");
+//         };
 
-leftBtn.onclick = function () {
-    replaceClass(backgroundsBanner[idx], "backgrounds-active", "");
-    idx -= 1;
-    if (idx < 0) {
-		idx = backgroundsBanner.length - 1;
-	}
+//         sliderBtns[i].classList.add("clients-list-carousel-item-active");
+//     });
 
-	backgroundsBanner[idx].classList.add('backgrounds-active');
-};
+// };
 
-rightBtn.onclick = function () {
-    replaceClass(backgroundsBanner[idx], "backgrounds-active", "");
-    idx += 1;
-    if (idx > backgroundsBanner.length - 1) {
-		idx = 0;
-	}
+// /////////////////////////////////////////////////////////////////////////
 
-	backgroundsBanner[idx].classList.add('backgrounds-active');
-};
+// let backgroundsBanner = document.getElementsByClassName("banner-backgrounds");
+// let leftBtn = document.getElementById("left-rectangle-arrow");
+// let rightBtn = document.getElementById("right-rectangle-arrow");
+
+// const replaceClass = (item, classItem, newClassItem) => {
+//     item.className = item.className.replace(classItem, newClassItem);
+// }
+
+// backgroundsBanner[0].classList.add("backgrounds-active");
+
+// let idx = 0;
+
+// leftBtn.onclick = function () {
+//     replaceClass(backgroundsBanner[idx], "backgrounds-active", "");
+//     idx -= 1;
+//     if (idx < 0) {
+// 		idx = backgroundsBanner.length - 1;
+// 	}
+
+// 	backgroundsBanner[idx].classList.add('backgrounds-active');
+// };
+
+// rightBtn.onclick = function () {
+//     replaceClass(backgroundsBanner[idx], "backgrounds-active", "");
+//     idx += 1;
+//     if (idx > backgroundsBanner.length - 1) {
+// 		idx = 0;
+// 	}
+
+// 	backgroundsBanner[idx].classList.add('backgrounds-active');
+// };
 
 /////////////////////////////////////////////////
+const BLOG_WRAPPER = document.getElementById("main-block-blog-wrapper");
+const BLOG_LENGHT = 3;
 
-const mainBlock = documnet.getElementById("about-us-nav");
+for (let el in news) {
+    const blog = document.createElement("div");
+    blog.classList.add("blogs");
+    BLOG_WRAPPER.append(blog);
+
+    const blogImg = document.createElement("div");
+    blogImg.classList.add("blogs-img");
+    blog.append(blogImg);
+
+    const img = document.createElement("img");
+    img.setAttribute("src", news[el].img)
+    blogImg.append(img);
+
+    const dateImg = document.createElement("div");
+    dateImg.classList.add("blogs-img-date");
+    dateImg.innerText = news[el].date;
+    blogImg.append(dateImg);
+
+    const heading = document.createElement("div");
+    heading.classList.add("blogs-img-heading");
+    blogImg.append(heading);
+    heading.innerText = news[el].title;
+
+    const actions = document.createElement("div");
+    actions.classList.add("actions");
+    blog.append(actions);
+
+    const btn = document.createElement("a");
+    btn.setAttribute("href", "#");
+    btn.classList.add("button");
+    btn.innerText = news[el].btn;
+    actions.append(btn);
+
+    const links = document.createElement("div");
+    links.classList.add("links");
+    actions.append(links);
+
+    const shareBlock = document.createElement("div");
+    shareBlock.classList.add("share-block");
+    shareBlock.classList.add("icon-share");
+    links.append(shareBlock);
+
+    const shareDate = document.createElement("span");
+    shareDate.classList.add("share-block-date");
+    shareDate.innerText = "13-feb-2016";
+    links.append(shareDate);
+    
+    const shareText = document.createElement("span");
+    shareText.classList.add("share-block-text");
+    shareText.innerText = "Share:";
+    links.append(shareText);
+
+    const iconsBlock = document.createElement("div");
+    iconsBlock.classList.add("icons-block");
+    actions.append(iconsBlock);
+
+    const linksIcon = document.createElement("a");
+    linksIcon.setAttribute("href", "#");
+    linksIcon.classList.add("icon-facebook");
+    iconsBlock.append(linksIcon);
+
+    const linksIcon2 = document.createElement("a");
+    linksIcon2.setAttribute("href", "#");
+    linksIcon2.classList.add("icon-twitter");
+    iconsBlock.append(linksIcon2);
+
+    const linksIcon3 = document.createElement("a");
+    linksIcon3.setAttribute("href", "#");
+    linksIcon3.classList.add("icon-gplus");
+    iconsBlock.append(linksIcon3);
+}
+///////////////////////////////////////////////////////////
+const shareBlock = document.getElementsByClassName("share-block");
+const iconsBlock = document.getElementsByClassName("icons-block");
+const shareBlockText = document.getElementsByClassName("share-block-text");
+const shareBlockDate = document.getElementsByClassName("share-block-date");
+
+const blogsLenght = document.getElementsByClassName("blogs").length;
+
+for (let i = 0; i < blogsLenght; i++) {
+    shareBlock[i].addEventListener("click", () => {
+        shareBlock[i].classList.toggle("share-block-active");
+        iconsBlock[i].classList.toggle("icons-block-active");
+        shareBlockDate[i].classList.toggle("share-block-date-active");
+        shareBlockText[i].classList.toggle("share-block-text-active");
+    });
+};
 
