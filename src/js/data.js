6d21627a5ee713dd05fdf1fbcd3e78970344b8a2
vladbyelik <vladbyelik@gmail.com>
2020-11-09 19:52:13 +0200

    lesson fixes 09.11

--- src/js/data.js
+++ src/js/data.js
@@ -1,114 +1,158 @@
 const data = [
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-1.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-2.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-3.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-1.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-2.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-3.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-1.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-2.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-3.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-1.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-2.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-3.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-1.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-2.png"
-	},
-	{
-		name: "Client Name",
-		position: "Designation",
-		img: "./assets/img/client-3.png"
-	}
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-1.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-2.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-3.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-1.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-2.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-3.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-1.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-2.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-3.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-1.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-2.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-3.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-1.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-2.png",
+  },
+  {
+    name: "Client Name",
+    position: "Designation",
+    img: "./assets/img/client-3.png",
+  },
 ];
 
 const news = [
-	{
-		type: 'new',
-		title: 'BLOG HEADING LOREM IPSUM IS SIMPLY DUMMY',
-		date: '11-feb-2016',
-		desc: 'Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
-		img: 'assets/img/blog-img.png',
-	},
-	{
-		type: 'new',
-		title: 'BLOG HEADING LOREM IPSUM IS SIMPLY DUMMY',
-		date: '11-feb-2016',
-		desc: 'Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
-		img: 'assets/img/blog-img-2.png',
-	},
-	{
-		type: 'audio',	
-		src: '../../file.mp3',
-		date: '13-feb-2016',
-		btn: "Watch more",
-		desc: 'When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
-	},
-	{
-		type: 'quote',
-		date: '13-feb-2016',
-		desc: 'Dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
-		btn: "Full quotes"
-	},	
-	{
-		type: 'new',
-		title: 'BLOG HEADING LOREM IPSUM IS SIMPLY DUMMY',
-		date: '11-feb-2016',
-		desc: 'Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
-		img: 'assets/img/blog-img-5.png',
-	},
-]
+  {
+    type: "new",
+    title: "BLOG HEADING LOREM IPSUM IS SIMPLY DUMMY",
+    date: "11-feb-2016",
+    desc:
+      "Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    img: "assets/img/blog-img.png",
+  },
+  {
+    type: "new",
+    title: "BLOG HEADING LOREM IPSUM IS SIMPLY DUMMY",
+    date: "11-feb-2016",
+    desc:
+      "Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    img: "assets/img/blog-img-2.png",
+  },
+  {
+    type: "audio",
+    src: "../../file.mp3",
+    date: "13-feb-2016",
+    btn: "Watch more",
+    desc:
+      "When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+  },
+  {
+    type: "quote",
+    date: "13-feb-2016",
+    desc:
+      "Dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+    btn: "Full quotes",
+  },
+  {
+    type: "new",
+    title: "BLOG HEADING LOREM IPSUM IS SIMPLY DUMMY",
+    date: "11-feb-2016",
+    desc:
+      "Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    img: "assets/img/blog-img-5.png",
+  },
+  {
+    type: "new",
+    title: "BLOG HEADING LOREM IPSUM IS SIMPLY DUMMY",
+    date: "11-feb-2016",
+    desc:
+      "Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    img: "assets/img/blog-img-2.png",
+  },
+  {
+    type: "audio",
+    src: "../../file.mp3",
+    date: "13-feb-2016",
+    btn: "Watch more",
+    desc:
+      "When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+  },
+  {
+    type: "quote",
+    date: "13-feb-2016",
+    desc:
+      "Dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+    btn: "Full quotes",
+  },
+  {
+    type: "new",
+    title: "BLOG HEADING LOREM IPSUM IS SIMPLY DUMMY",
+    date: "11-feb-2016",
+    desc:
+      "Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    img: "assets/img/blog-img.png",
+  },
+  {
+    type: "new",
+    title: "BLOG HEADING LOREM IPSUM IS SIMPLY DUMMY",
+    date: "11-feb-2016",
+    desc:
+      "Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    img: "assets/img/blog-img-2.png",
+  },
+];
--- src/js/page.js
+++ src/js/page.js
@@ -1,120 +1,90 @@
-const BLOG_WRAPPER = document.getElementById("main-block-blog-wrapper"); 
+const BLOG_WRAPPER = document.getElementById("main-block-blog-wrapper");
 
-const madeBlock = (nameEl, parEl, className, innerTextEl = null, atributeName = null, atributeItem = null) => {
-    nameEl.classList.add(className);
-    parEl.append(nameEl);
-    nameEl.innerText = innerTextEl;
-    nameEl.setAttribute(atributeName, atributeItem);
-}
+console.log(news);
+
+const madeBlock = (
+  nameEl,
+  parEl,
+  className,
+  innerTextEl = null,
+  atributeName = null,
+  atributeItem = null
+) => {
+  nameEl.className = className;
+  parEl.append(nameEl);
+  nameEl.innerText = innerTextEl;
+  nameEl.setAttribute(atributeName, atributeItem);
+};
+
+const navigateElements = (blog, date) => {
+  const actions = document.createElement("div");
+  const btn = document.createElement("a");
+  const links = document.createElement("div");
+  const shareBlock = document.createElement("div");
+  const shareDate = document.createElement("span");
+  const shareText = document.createElement("span");
+  const iconsBlock = document.createElement("div");
+  const linksIcon = document.createElement("a");
+  const linksIcon2 = document.createElement("a");
+  const linksIcon3 = document.createElement("a");
+  madeBlock(actions, blog, "actions");
+  madeBlock(btn, actions, "button", "Read more", "href", "#");
+  madeBlock(links, actions, "links");
+  madeBlock(shareBlock, links, "share-block icon-share");
+  madeBlock(shareDate, links, "share-block-date", date);
+  madeBlock(shareText, links, "share-block-text", "Share:");
+  madeBlock(iconsBlock, actions, "icons-block");
+  madeBlock(linksIcon, iconsBlock, "icon-facebook", "", "href", "#");
+  madeBlock(linksIcon2, iconsBlock, "icon-twitter", "", "href", "#");
+  madeBlock(linksIcon3, iconsBlock, "icon-gplus", "", "href", "#");
+};
 
 for (let el in news) {
+  if (news[el].type === "audio") {
+    const blog = document.createElement("div");
+    const audio = document.createElement("audio");
+    const audioSource = document.createElement("source");
+    const blogDesc = document.createElement("div");
+    madeBlock(blog, BLOG_WRAPPER, "blogs");
+    madeBlock(audio, blog, "", "", "controls", "#");
+    madeBlock(audioSource, audio, "", "", "src", "#");
+    madeBlock(blogDesc, blog, "blogs-description", news[el].desc);
+    navigateElements(blog, news[el].date);
+  }
 
-    if (news[el].type === "audio") {
-            
-        const blog = document.createElement("div");
-        const audio = document.createElement("audio");
-        const audioSource = document.createElement("source"); 
-        const blogDesc = document.createElement('div');
-        const actions = document.createElement("div");
-        const btn = document.createElement("a");
-        const links = document.createElement("div");
-        const shareBlock = document.createElement("div");
-        const shareDate = document.createElement("span");
-        const shareText = document.createElement("span");
-        const iconsBlock = document.createElement("div");
-        const linksIcon = document.createElement("a");
-        const linksIcon2 = document.createElement("a");
-        const linksIcon3 = document.createElement("a");
+  if (news[el].type === "new") {
+    const blog = document.createElement("div");
+    const blogImg = document.createElement("div");
+    const img = document.createElement("img");
+    const dateImg = document.createElement("div");
+    const heading = document.createElement("div");
+    const blogDesc = document.createElement("div");
+    madeBlock(blog, BLOG_WRAPPER, "blogs");
+    madeBlock(blogImg, blog, "blogs-img");
+    madeBlock(img, blogImg, "#", "", "src", news[el].img);
+    madeBlock(dateImg, blogImg, "blogs-img-date", news[el].date);
+    madeBlock(heading, blogImg, "blogs-img-heading", news[el].title);
+    madeBlock(blogDesc, blog, "blogs-description", news[el].desc);
+    navigateElements(blog, news[el].date);
+  }
 
-        madeBlock(blog, BLOG_WRAPPER, "blogs");
-        madeBlock(audio, blog, "#", "", "controls", "#");
-        madeBlock(audioSource, audio, "#", "", "src", "#");
-        madeBlock(blogDesc, blog, "blogs-description", news[el].desc);
-        madeBlock(actions, blog, "actions");
-        madeBlock(btn, actions, "button", "Read more", "href", "#");
-        madeBlock(links, actions, "links");
-        madeBlock(shareBlock, links, "share-block");
-        shareBlock.classList.add("icon-share");
-        madeBlock(shareDate, links, "share-block-date", news[el].date);
-        madeBlock(shareText, links, "share-block-text", "Share:");
-        madeBlock(iconsBlock, actions, "icons-block");
-        madeBlock(linksIcon, iconsBlock, "icon-facebook", "", "href", "#");
-        madeBlock(linksIcon2, iconsBlock, "icon-twitter", "", "href", "#");
-        madeBlock(linksIcon3, iconsBlock, "icon-gplus", "", "href", "#");
-    }
+  if (news[el].type === "quote") {
+    const blog = document.createElement("div");
+    const blockQuote = document.createElement("div");
+    const blueBlockWrapper = document.createElement("div");
+    const blueBlockWrapperText = document.createElement("p");
+    madeBlock(blog, BLOG_WRAPPER, "blogs");
+    madeBlock(blockQuote, blog, "blue-block");
+    madeBlock(blueBlockWrapper, blockQuote, "blue-block-wrapper");
+    madeBlock(
+      blueBlockWrapperText,
+      blueBlockWrapper,
+      "blue-block-wrapper-text",
+      news[el].desc
+    );
 
-    if (news[el].type === "new") {
-
-
-        const blog = document.createElement("div");
-        const blogImg = document.createElement("div");
-        const img = document.createElement("img");
-        const dateImg = document.createElement("div");
-        const heading = document.createElement("div");
-        const blogDesc = document.createElement('div');
-        const actions = document.createElement("div");
-        const btn = document.createElement("a");
-        const links = document.createElement("div");
-        const shareBlock = document.createElement("div");
-        const shareDate = document.createElement("span");
-        const shareText = document.createElement("span");
-        const iconsBlock = document.createElement("div");
-        const linksIcon = document.createElement("a");
-        const linksIcon2 = document.createElement("a");
-        const linksIcon3 = document.createElement("a");
-
-        madeBlock(blog, BLOG_WRAPPER, "blogs");
-        madeBlock(blogImg, blog, "blogs-img");
-        madeBlock(img, blogImg, "#", "", "src", news[el].img);
-        madeBlock(dateImg, blogImg, "blogs-img-date", news[el].date);
-        madeBlock(heading, blogImg, "blogs-img-heading", news[el].title);
-        madeBlock(blogDesc, blog, "blogs-description", news[el].desc);
-        madeBlock(actions, blog, "actions");
-        madeBlock(btn, actions, "button", "Read more", "href", "#");
-        madeBlock(links, actions, "links");
-        madeBlock(shareBlock, links, "share-block");
-        shareBlock.classList.add("icon-share");
-        madeBlock(shareDate, links, "share-block-date", news[el].date);
-        madeBlock(shareText, links, "share-block-text", "Share:");
-        madeBlock(iconsBlock, actions, "icons-block");
-        madeBlock(linksIcon, iconsBlock, "icon-facebook", "", "href", "#");
-        madeBlock(linksIcon2, iconsBlock, "icon-twitter", "", "href", "#");
-        madeBlock(linksIcon3, iconsBlock, "icon-gplus", "", "href", "#");
-
-    }
-    
-    if (news[el].type === "quote") {
-        const blog = document.createElement("div");
-        const blueBlock = document.createElement("div");
-        const blueBlockWrapper = document.createElement("div");
-        const blueBlockWrapperText = document.createElement("p");
-        const actions = document.createElement("div");
-        const btn = document.createElement("a");
-        const links = document.createElement("div");
-        const shareBlock = document.createElement("div");
-        const shareDate = document.createElement("span");
-        const shareText = document.createElement("span");
-        const iconsBlock = document.createElement("div");
-        const linksIcon = document.createElement("a");
-        const linksIcon2 = document.createElement("a");
-        const linksIcon3 = document.createElement("a");
-
-        madeBlock(blog, BLOG_WRAPPER, "blogs");
-        madeBlock(blueBlock, blog, "blue-block");
-        madeBlock(blueBlockWrapper, blueBlock, "blue-block-wrapper");
-        madeBlock(blueBlockWrapperText, blueBlockWrapper, "blue-block-wrapper-text", news[el].desc)
-        madeBlock(actions, blog, "actions");
-        madeBlock(btn, actions, "button", "Read more", "href", "#");
-        madeBlock(links, actions, "links");
-        madeBlock(shareBlock, links, "share-block");
-        shareBlock.classList.add("icon-share");
-        madeBlock(shareDate, links, "share-block-date", news[el].date);
-        madeBlock(shareText, links, "share-block-text", "Share:");
-        madeBlock(iconsBlock, actions, "icons-block");
-        madeBlock(linksIcon, iconsBlock, "icon-facebook", "", "href", "#");
-        madeBlock(linksIcon2, iconsBlock, "icon-twitter", "", "href", "#");
-        madeBlock(linksIcon3, iconsBlock, "icon-gplus", "", "href", "#");
-    }
+    navigateElements(blog, news[el].date);
+  }
 }
 ///////////////////////////////////////////////////////////
 
@@ -122,15 +92,13 @@ const shareBlock = document.getElementsByClassName("share-block");
 const iconsBlock = document.getElementsByClassName("icons-block");
 const shareBlockText = document.getElementsByClassName("share-block-text");
 const shareBlockDate = document.getElementsByClassName("share-block-date");
+const blogsLength = document.getElementsByClassName("blogs").length;
 
-const blogsLenght = document.getElementsByClassName("blogs").length;
-
-for (let i = 0; i < blogsLenght - 1; i++) {
-    shareBlock[i].addEventListener("click", () => {
-        shareBlock[i].classList.toggle("share-block-active");
-        iconsBlock[i].classList.toggle("icons-block-active");
-        shareBlockDate[i].classList.toggle("share-block-date-active");
-        shareBlockText[i].classList.toggle("share-block-text-active");
-    });
-};
-
+for (let i = 0; i < blogsLength; i++) {
+  shareBlock[i].addEventListener("click", () => {
+    shareBlock[i].classList.toggle("share-block-active");
+    iconsBlock[i].classList.toggle("icons-block-active");
+    shareBlockDate[i].classList.toggle("share-block-date-active");
+    shareBlockText[i].classList.toggle("share-block-text-active");
+  });
+}
