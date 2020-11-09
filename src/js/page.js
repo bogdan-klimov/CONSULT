2e4664bcb52f830719a65547e897ea0b900cc43a
bogdan-klimov <bodyaklimov333@gmail.com>
2020-11-09 11:34:55 +0200

    OptimizationJsCode

--- src/js/page.js
+++ src/js/page.js
@@ -1,219 +1,119 @@
-const BLOG_WRAPPER = document.getElementById("main-block-blog-wrapper");
+const BLOG_WRAPPER = document.getElementById("main-block-blog-wrapper"); 
+
+const madeBlock = (nameEl, parEl, className, innerTextEl = null, atributeName = null, atributeItem = null) => {
+    nameEl.classList.add(className);
+    parEl.append(nameEl);
+    nameEl.innerText = innerTextEl;
+    nameEl.setAttribute(atributeName, atributeItem);
+}
 
 for (let el in news) {
 
     if (news[el].type === "audio") {
-			
+            
         const blog = document.createElement("div");
-        blog.classList.add("blogs");
-        BLOG_WRAPPER.append(blog);
-
         const audio = document.createElement("audio");
-        audio.setAttribute("controls", "#");
-        blog.append(audio);
-
-        const audioSource = document.createElement("source")
-        audioSource.setAttribute("src", "#")
-        audio.append(audioSource);
-
+        const audioSource = document.createElement("source"); 
         const blogDesc = document.createElement('div');
-        blogDesc.classList.add("blogs-description");
-        blogDesc.innerText = news[el].desc;
-        blog.append(blogDesc);
-
         const actions = document.createElement("div");
-        actions.classList.add("actions");
-        blog.append(actions);
-
         const btn = document.createElement("a");
-        btn.setAttribute("href", "#");
-        btn.classList.add("button");
-        btn.innerText = news[el].btn;
-        actions.append(btn);
-
         const links = document.createElement("div");
-        links.classList.add("links");
-        actions.append(links);
-
         const shareBlock = document.createElement("div");
-        shareBlock.classList.add("share-block");
-        shareBlock.classList.add("icon-share");
-        links.append(shareBlock);
-
         const shareDate = document.createElement("span");
-        shareDate.classList.add("share-block-date");
-        shareDate.innerText = news[el].date;
-        links.append(shareDate);
-        
         const shareText = document.createElement("span");
-        shareText.classList.add("share-block-text");
-        shareText.innerText = "Share:";
-        links.append(shareText);
-
         const iconsBlock = document.createElement("div");
-        iconsBlock.classList.add("icons-block");
-        actions.append(iconsBlock);
-
         const linksIcon = document.createElement("a");
-        linksIcon.setAttribute("href", "#");
-        linksIcon.classList.add("icon-facebook");
-        iconsBlock.append(linksIcon);
-
         const linksIcon2 = document.createElement("a");
-        linksIcon2.setAttribute("href", "#");
-        linksIcon2.classList.add("icon-twitter");
-        iconsBlock.append(linksIcon2);
-
         const linksIcon3 = document.createElement("a");
-        linksIcon3.setAttribute("href", "#");
-        linksIcon3.classList.add("icon-gplus");
-        iconsBlock.append(linksIcon3);
+
+        madeBlock(blog, BLOG_WRAPPER, "blogs");
+        madeBlock(audio, blog, "#", "", "controls", "#");
+        madeBlock(audioSource, audio, "#", "", "src", "#");
+        madeBlock(blogDesc, blog, "blogs-description", news[el].desc);
+        madeBlock(actions, blog, "actions");
+        madeBlock(btn, actions, "button", "Read more", "href", "#");
+        madeBlock(links, actions, "links");
+        madeBlock(shareBlock, links, "share-block");
+        shareBlock.classList.add("icon-share");
+        madeBlock(shareDate, links, "share-block-date", news[el].date);
+        madeBlock(shareText, links, "share-block-text", "Share:");
+        madeBlock(iconsBlock, actions, "icons-block");
+        madeBlock(linksIcon, iconsBlock, "icon-facebook", "", "href", "#");
+        madeBlock(linksIcon2, iconsBlock, "icon-twitter", "", "href", "#");
+        madeBlock(linksIcon3, iconsBlock, "icon-gplus", "", "href", "#");
     }
 
     if (news[el].type === "new") {
 
+
         const blog = document.createElement("div");
-        blog.classList.add("blogs");
-        BLOG_WRAPPER.append(blog);
-
         const blogImg = document.createElement("div");
-        blogImg.classList.add("blogs-img");
-        blog.append(blogImg);
-
         const img = document.createElement("img");
-        img.setAttribute("src", news[el].img)
-        blogImg.append(img);
-
         const dateImg = document.createElement("div");
-        dateImg.classList.add("blogs-img-date");
-        dateImg.innerText = news[el].date;
-        blogImg.append(dateImg);
-
         const heading = document.createElement("div");
-        heading.classList.add("blogs-img-heading");
-        blogImg.append(heading);
-        heading.innerText = news[el].title;
-
         const blogDesc = document.createElement('div');
-        blogDesc.classList.add("blogs-description");
-        blogDesc.innerText = news[el].desc;
-        blog.append(blogDesc);
-
         const actions = document.createElement("div");
-        actions.classList.add("actions");
-        blog.append(actions);
-
         const btn = document.createElement("a");
-        btn.setAttribute("href", "#");
-        btn.classList.add("button");
-        btn.innerText = 'Read more';
-        actions.append(btn);
-
         const links = document.createElement("div");
-        links.classList.add("links");
-        actions.append(links);
-
         const shareBlock = document.createElement("div");
-        shareBlock.classList.add("share-block");
-        shareBlock.classList.add("icon-share");
-        links.append(shareBlock);
-
         const shareDate = document.createElement("span");
-        shareDate.classList.add("share-block-date");
-        shareDate.innerText = news[el].date;
-        links.append(shareDate);
-        
         const shareText = document.createElement("span");
-        shareText.classList.add("share-block-text");
-        shareText.innerText = "Share:";
-        links.append(shareText);
-
         const iconsBlock = document.createElement("div");
-        iconsBlock.classList.add("icons-block");
-        actions.append(iconsBlock);
-
         const linksIcon = document.createElement("a");
-        linksIcon.setAttribute("href", "#");
-        linksIcon.classList.add("icon-facebook");
-        iconsBlock.append(linksIcon);
-
         const linksIcon2 = document.createElement("a");
-        linksIcon2.setAttribute("href", "#");
-        linksIcon2.classList.add("icon-twitter");
-        iconsBlock.append(linksIcon2);
-
         const linksIcon3 = document.createElement("a");
-        linksIcon3.setAttribute("href", "#");
-        linksIcon3.classList.add("icon-gplus");
-        iconsBlock.append(linksIcon3);
+
+        madeBlock(blog, BLOG_WRAPPER, "blogs");
+        madeBlock(blogImg, blog, "blogs-img");
+        madeBlock(img, blogImg, "#", "", "src", news[el].img);
+        madeBlock(dateImg, blogImg, "blogs-img-date", news[el].date);
+        madeBlock(heading, blogImg, "blogs-img-heading", news[el].title);
+        madeBlock(blogDesc, blog, "blogs-description", news[el].desc);
+        madeBlock(actions, blog, "actions");
+        madeBlock(btn, actions, "button", "Read more", "href", "#");
+        madeBlock(links, actions, "links");
+        madeBlock(shareBlock, links, "share-block");
+        shareBlock.classList.add("icon-share");
+        madeBlock(shareDate, links, "share-block-date", news[el].date);
+        madeBlock(shareText, links, "share-block-text", "Share:");
+        madeBlock(iconsBlock, actions, "icons-block");
+        madeBlock(linksIcon, iconsBlock, "icon-facebook", "", "href", "#");
+        madeBlock(linksIcon2, iconsBlock, "icon-twitter", "", "href", "#");
+        madeBlock(linksIcon3, iconsBlock, "icon-gplus", "", "href", "#");
+
     }
     
     if (news[el].type === "quote") {
         const blog = document.createElement("div");
-        blog.classList.add("blogs");
-        BLOG_WRAPPER.append(blog);
-
         const blueBlock = document.createElement("div");
-        blueBlock.classList.add("blue-block");
-        blog.append(blueBlock);
-
         const blueBlockWrapper = document.createElement("div");
-        blueBlockWrapper.classList.add("blue-block-wrapper");
-        blueBlock.append(blueBlockWrapper);
-
         const blueBlockWrapperText = document.createElement("p");
-        blueBlockWrapperText.classList.add("blue-block-wrapper-text")
-        blueBlockWrapperText.innerText = news[el].desc;
-        blueBlockWrapper.append(blueBlockWrapperText);
-
         const actions = document.createElement("div");
-        actions.classList.add("actions");
-        blog.append(actions);
-
         const btn = document.createElement("a");
-        btn.setAttribute("href", "#");
-        btn.classList.add("button");
-        btn.innerText = news[el].btn;
-        actions.append(btn);
-
         const links = document.createElement("div");
-        links.classList.add("links");
-        actions.append(links);
-
         const shareBlock = document.createElement("div");
-        shareBlock.classList.add("share-block");
-        shareBlock.classList.add("icon-share");
-        links.append(shareBlock);
-
         const shareDate = document.createElement("span");
-        shareDate.classList.add("share-block-date");
-        shareDate.innerText = news[el].date;
-        links.append(shareDate);
-        
         const shareText = document.createElement("span");
-        shareText.classList.add("share-block-text");
-        shareText.innerText = "Share:";
-        links.append(shareText);
-
         const iconsBlock = document.createElement("div");
-        iconsBlock.classList.add("icons-block");
-        actions.append(iconsBlock);
-
         const linksIcon = document.createElement("a");
-        linksIcon.setAttribute("href", "#");
-        linksIcon.classList.add("icon-facebook");
-        iconsBlock.append(linksIcon);
-
         const linksIcon2 = document.createElement("a");
-        linksIcon2.setAttribute("href", "#");
-        linksIcon2.classList.add("icon-twitter");
-        iconsBlock.append(linksIcon2);
-
         const linksIcon3 = document.createElement("a");
-        linksIcon3.setAttribute("href", "#");
-        linksIcon3.classList.add("icon-gplus");
-        iconsBlock.append(linksIcon3);
 
+        madeBlock(blog, BLOG_WRAPPER, "blogs");
+        madeBlock(blueBlock, blog, "blue-block");
+        madeBlock(blueBlockWrapper, blueBlock, "blue-block-wrapper");
+        madeBlock(blueBlockWrapperText, blueBlockWrapper, "blue-block-wrapper-text", news[el].desc)
+        madeBlock(actions, blog, "actions");
+        madeBlock(btn, actions, "button", "Read more", "href", "#");
+        madeBlock(links, actions, "links");
+        madeBlock(shareBlock, links, "share-block");
+        shareBlock.classList.add("icon-share");
+        madeBlock(shareDate, links, "share-block-date", news[el].date);
+        madeBlock(shareText, links, "share-block-text", "Share:");
+        madeBlock(iconsBlock, actions, "icons-block");
+        madeBlock(linksIcon, iconsBlock, "icon-facebook", "", "href", "#");
+        madeBlock(linksIcon2, iconsBlock, "icon-twitter", "", "href", "#");
+        madeBlock(linksIcon3, iconsBlock, "icon-gplus", "", "href", "#");
     }
 }
 ///////////////////////////////////////////////////////////
@@ -225,7 +125,7 @@ const shareBlockDate = document.getElementsByClassName("share-block-date");
 
 const blogsLenght = document.getElementsByClassName("blogs").length;
 
-for (let i = 0; i < blogsLenght; i++) {
+for (let i = 0; i < blogsLenght - 1; i++) {
     shareBlock[i].addEventListener("click", () => {
         shareBlock[i].classList.toggle("share-block-active");
         iconsBlock[i].classList.toggle("icons-block-active");
